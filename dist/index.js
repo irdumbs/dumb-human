377a9588b378fa742494ebf31022eb085edf5d50
irdumb <go2knox@hotmail.com>
2015-11-04 20:18:58 -0500

    Update

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,34 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var HelloWorld = function HelloWorld() {
-  _classCallCheck(this, HelloWorld);
+function _instanceof(left, right) { if (right != null && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+var DumbHuman = function DumbHuman() {
+  _classCallCheck(this, DumbHuman);
+
+  var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+  if (!_instanceof(this, DumbHuman)) {
+    return new DumbHuman(options);
+  }
+
+  // '/Users/username', '/home/username'
+  this.options = _lodash2.default.defaults(options, {
+    configFile: ''
+  });
+
+  this.name = this.options.name;
 };
+
+exports.default = DumbHuman;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-class HelloWorld {}
+import { EventEmitter } from 'events'
+import _ from 'lodash'
+
+export default class DumbHuman extends EventEmitter {
+
+  constructor (options = {}) {
+    super()
+
+    this.options = _.defaults(options, {
+      configFile: ''
+    })
+
+    this.name = this.options.name
+  }
+
+}
